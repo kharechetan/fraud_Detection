146c27e8f83992155bf9d3c8858123d9d22cbafb
kharechetan <khare.chetan@gmail.com>
2022-01-15 22:27:43 +0530

    added 3 new ml model code

--- ml_model.py
+++ ml_model.py
@@ -2,3 +2,14 @@
 
 print("Adding all ML related code here")
 
+def model1():
+    print("Adding code for ML Model1")
+
+
+def model2():
+    print("Adding code for ML Model2")
+
+
+def model3():
+    print("Adding code for ML Model3")
+
